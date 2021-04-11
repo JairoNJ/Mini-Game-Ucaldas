8455d5bb93456a9e2251b03fe8a79d66c786e5b6
movz <jairoanglus@gmail.com>
2021-04-10 23:43:51 -0500

    Primera version del Mini-Game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+    public float maxSpeed = 10f;
+    public bool isFloor;
+    public float radio = 0.8f;
+    public LayerMask floor;
+    public Transform pie;
+    public float maxJump = 40f;
+
+    public Transform Player;
+    public float posX;
+   
+
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DirectionPlayer();
+        JumpPlayer();
+        WallsPositions();
+        
+    }
+
+    public void DirectionPlayer()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(direction * maxSpeed, body.velocity.y);
+
+        if(direction > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if(direction < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+    public void JumpPlayer()
+    {
+        isFloor = Physics2D.OverlapCircle(pie.position, radio, floor);
+        if (isFloor)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                body.AddForce(new Vector2(0f, maxJump * 20));
+            }
+        }
+    }
+
+    public void WallsPositions()
+    {
+        float posX = transform.position.x;
+        if(posX >= 8)
+        {
+            transform.position = new Vector3(-8, -3, 0);
+        }
+        if(posX <= -8)
+        {
+            transform.position = new Vector3(8, -3, 0);
+        }
+    }
+
+   
+
+    
+
+
+}
---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+    
+    public Transform Player;
+    public Transform pointA;
+    public Transform pointB;
+    
+
+   
+
+    void Start()
+    {
+        InitElements();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void InitElements()
+    {
+        
+        Debug.Log("POINT-A: " + pointA.position);
+        Debug.Log("POINT-B: " + pointB.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "PointA")
+        {
+            Player.position = pointA.transform.position;
+            Debug.Log("ENTRASTE");
+        }
+    }
+}
